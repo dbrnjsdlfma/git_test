b6bdd8309a64aff912fadf6399e9ab02ac7e91d3
big2-16 <big2-16@big2-16-PC>
2018-07-10 12:10:42 +0900

    modify : Main class method : main()

--- git_test/src/git_test/Main.java
+++ git_test/src/git_test/Main.java
@@ -3,6 +3,7 @@ package git_test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello JSP");
+		System.out.println("Hello HTML");
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello java!");
